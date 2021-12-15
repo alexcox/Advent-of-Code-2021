2ede75cdc7f6a06a14c75a4704629036caea260f
Alex Cox <5402568+AlexCox@users.noreply.github.com>
2021-12-14 22:31:18 -0500

    Day 14
    Refactoring

--- day14.py
+++ day14.py
@@ -1,17 +1,15 @@
-input = [x.split('\n') for x in open('input/day14.txt', 'r').read().split('\n\n')]
-polymer, insertion_rules = input[0][0], dict(k.split(' -> ') for k in input[1])
+polymer, input = open('input/day14.txt', 'r').read().split('\n\n')
+insertion_rules, keys = dict(k.split(' -> ') for k in input.split('\n')), list('BCHNOVPKSF')
 
 def polymer_difference(steps = 10):
-    keys = list('BCHNOVPKSF')
     count = {k: polymer.count(k) for k in keys}
     pairs = {i + j: polymer.count(i + j) for i in keys for j in keys}
     for step in range(steps):
         d = {}
         for pair in pairs:
-            if pairs[pair] > 0:
-                el, n, pairs[pair] = insertion_rules[pair], pairs[pair], 0
-                count[el] += n
-                d[pair[0] + el], d[el + pair[1]] = d.get(pair[0] + el, 0) + n, d.get(el + pair[1], 0) + n
+            el, n, pairs[pair] = insertion_rules[pair], pairs[pair], 0
+            count[el] += n
+            d[pair[0] + el], d[el + pair[1]] = d.get(pair[0] + el, 0) + n, d.get(el + pair[1], 0) + n
         for k in d: pairs[k] += d[k]
     return max(count.values()) - min(count.values())
 
