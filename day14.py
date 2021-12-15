21ca1968431eacd368c026a07be19c66534a5957
Alex Cox <5402568+AlexCox@users.noreply.github.com>
2021-12-14 22:33:11 -0500

    Day 14
    Refactoring

--- day14.py
+++ day14.py
@@ -10,7 +10,7 @@ def polymer_difference(steps = 10):
             el, n, pairs[pair] = insertion_rules[pair], pairs[pair], 0
             count[el] += n
             d[pair[0] + el], d[el + pair[1]] = d.get(pair[0] + el, 0) + n, d.get(el + pair[1], 0) + n
-        for k in d: pairs[k] += d[k]
+        pairs |= d
     return max(count.values()) - min(count.values())
 
 print(f'Solution to part 1: {polymer_difference()}')
